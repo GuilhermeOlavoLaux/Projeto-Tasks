e1523c8c48d64bc6b58c1db74a0cbbe232cd6e39
Guilherme Olavo Laux <guilherme.laux@compliancetotal.com.br>
2021-09-29 00:01:52 -0300

    corrige funcionalidade de deletar

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,9 @@ import Tasks from './Components/Tasks'
 function App() {
  
   return (
-    <>
+    <div className='body-container'>
       <Tasks></Tasks>
-    </>
+    </div>
   )
 }
 
--- src/Components/Tasks.tsx
+++ src/Components/Tasks.tsx
@@ -2,26 +2,29 @@ import React from 'react'
 import Task from './Task'
 
 export default class Tasks extends React.Component {
-  state = {
-    tasksList: [
-      { id: 1, name: 'Guilherme' },
-      { id: 2, name: 'Joe' }
-    ]
-  }
-  
   constructor(props: any) {
     super(props)
-    
+    this.state = {
+      tasksList: [
+        { id: '1', name: 'Guilherme' },
+        { id: 2, name: 'Joe' }
+      ]
+    }
+
+    this.handleTaskDelete = this.handleTaskDelete.bind(this)
   }
-  handleTaskDelete(taskId: any)  {
-    console.log('aaaaa', this.state)
-    // const b = this.state.tasksList.filter((task: any) => task.id !== taskId )
+
+  handleTaskDelete(taskId: any) {
+    //@ts-ignore
+    const newList = this.state.tasksList.filter(
+      (task: any) => task.id !== taskId
+    )
+    this.setState({ tasksList: newList })
   }
+
   render() {
     return (
       <div className='tasks-container'>
-        <p>O vo mata o TypeScript</p>
-
         {
           //@ts-ignore
           this.state.tasksList.map((task: any) => (
