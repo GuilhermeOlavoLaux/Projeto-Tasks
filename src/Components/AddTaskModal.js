528af03996e069b100f603eb44b7beec9e39ef3f
Guilherme Olavo Laux <guilherme.laux@compliancetotal.com.br>
2021-10-08 00:06:44 -0300

    adiciona css da modal de adicionar task

--- src/Components/AddTaskModal.js
+++ src/Components/AddTaskModal.js
@@ -21,7 +21,27 @@ export default function AddTask(props) {
         <Modal.Body className='add-task-modal'>
           <div className='add-task-container'>
             <h1>Adicionar Tarefa</h1>
-            <Button onClick={handleClose}>Fechar</Button>
+
+
+            <div className='id-input-container'>
+              <p>ID:</p>
+              <input></input>
+            </div>
+
+            <div className='name-input-container'>
+              <p>Nome:</p>
+              <input></input>
+            </div>
+
+            <div className='description-input-container'>
+              <p>Descrição:</p>
+              <textarea></textarea>
+            </div>
+
+            <div className='buttons-container'>
+              <Button onClick={handleClose}>Fechar</Button>
+              <Button onClick={handleClose}>Salvar</Button>
+            </div>
           </div>
         </Modal.Body>
       </Modal>
